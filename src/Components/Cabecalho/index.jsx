9e8ff81e310f1f18317e0620925cb31f512e8a2a
Luca-Ferri <lucaferrip@gmail.com>
2022-11-08 16:11:10 -0300

    Refatorando Cabecalho para usar a biblioteca styled-component

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,19 +1,58 @@
 import React from "react";
+import styled from "styled-components";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import { corPrimaria } from "./../UI/variaveis";
+
+const StyledHeader = styled.nav`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-around;
+  padding: 20px;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`;
+
+const BtnCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background-color: ${
+    ({ backGroungColor }) => backGroungColor ? backGroungColor : 'transparent'
+  };
+  color: ${
+    ({ color }) => color ? color : 'white'
+  }
+`;
 
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
-      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
+    <StyledHeader>
+      <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnCabecalho
+          href="https://google.com"
+          backGroungColor='white'
+          color={corPrimaria}
+        >
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnCabecalho>
+        <BtnCabecalho 
+          href="https://google.com"
+          backGroungColor={corPrimaria}
+          color='white'
+        >
           Sair
-        </a>
+        </BtnCabecalho>
       </div>
-    </div>
+    </StyledHeader>
   );
 };
 
